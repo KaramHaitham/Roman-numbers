76732646ae11ed7810ebfb012d69845eec79b6e5
Karam Mohd <k.haytham.mohammed@octo.com>
2021-11-22 11:37:24 +0100

    fixup! Add algorithm that covers 8 to 13

--- test/test.js
+++ test/test.js
@@ -6,27 +6,35 @@ const { expect } = require('chai');
  * create a set of roman numbers 2 -> II - ok
  * create a set of roman numbers 3 -> III - ok
  * create a set of roman numbers 4 -> IV - ok
- * create a set of roman numbers 5 -> V
- * create a set of roman numbers 6,7,8 -> VI,VII,VIII
- * create a set of roman numbers 9,10 -> IX, X
- * create a set of roman numbers 11,12,13 -> XI, XII , XIII
+ * create a set of roman numbers 5 -> V - ok
+ * create a set of roman numbers 6,7,8 -> VI,VII,VIII - ok
+ * create a set of roman numbers 9,10 -> IX, X - ok
+ * create a set of roman numbers 11,12,13 -> XI, XII , XIII - ok 
+ * create a set of roman numbers 14,15,16 -> XIV, XV , XVI 
 
  */
 
+ const baseMapping = { 
+  [0]:'',
+  [5]:'V',
+  [10]:'X' 
+}
 
 function convert(num) {
   if(num == 4) return 'IV';
   if(num == 9) return 'IX';
+  if(num == 14) return 'XIV';
+
+  let base = 10;
 
   if(num <= 3) {
-    return process(0,num,'');
+    base = 0;
+  }
+  else if(num < 9) {
+    base = 5;
   }
 
-  if(num < 9) {
-    return process(5,num,'V');
-  }
-
-  return process(10,num,'X');
+  return process(base,num,baseMapping[base]);
 }
 
 function process ( start , num , result ) {
@@ -143,6 +151,50 @@ it('Returns the char X for number 10', function() {
   //when
   const result = convert(10);
 
+  //then
+  expect(result).to.equal(expected);
+})
+
+it('Returns the char XI for number 11', function() {
+  //given
+  const expected = 'XI';
+  
+  //when
+  const result = convert(11);
+  
+  //then
+  expect(result).to.equal(expected);
+})
+
+it('Returns the char XII for number 12', function() {
+  //given
+  const expected = 'XII';
+  
+  //when
+  const result = convert(12);
+  
+  //then
+  expect(result).to.equal(expected);
+})
+
+it('Returns the char XIII for number 13', function() {
+  //given
+  const expected = 'XIII';
+  
+  //when
+  const result = convert(13);
+  
+  //then
+  expect(result).to.equal(expected);
+})
+
+it('Returns the char XIV for number 14', function() {
+  //given
+  const expected = 'XIV';
+  
+  //when
+  const result = convert(14);
+  
   //then
   expect(result).to.equal(expected);
 })
